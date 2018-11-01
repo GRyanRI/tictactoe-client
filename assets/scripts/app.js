3897d1875e2e2a3e2dfa2f6222b928a43e5c858b
GRyanRI <gregorywryan@gmail.com>
2018-11-01 16:39:39 -0400

    Completed the game board layout. made squares clickable. Added X's and O's Used jQuery, HTML, and SCSS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,23 @@
 // require('./example')
 
 $(() => {
+  const player1 = 'X'
+  const player2 = 'O'
 
-}
-)
+  let currentTurn = 1
+  let moves = 0
+  let square = $('.gameSquare')
+
+  square.on('click', function (c) {
+    moves++
+    if (currentTurn === 1) {
+      event.target.innerHTML = player1
+      event.target.style.color = 'red'
+      currentTurn++
+    } else {
+      event.target.innerHTML = player2
+      event.target.style.color = 'gray'
+      currentTurn--
+    }
+  })
+})
